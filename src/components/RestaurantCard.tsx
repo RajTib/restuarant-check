76598e75f384132f9889d5c4ca1fcab9d5837a1c
RajTib <raj.tibarewala@gmail.com>
2026-04-22 04:57:01 +0000

    Added Colour to cards and hover effect

--- src/components/RestaurantCard.tsx
+++ src/components/RestaurantCard.tsx
@@ -11,9 +11,15 @@ function getScoreColour(score: number): string {
     return "red";
 }
 
+function getCardColour(score: number): string {
+    if (score >= 80) return "#95d669ec"
+    if (50 <= score && score <= 79) return "#fdca86"
+    return "#ffaaaa"
+}
+
 function RestaurantCard({ restaurant }: Props) {
     return (
-        <div className="restaurantCard">
+        <div className="restaurantCard" style={{ backgroundColor: getCardColour(restaurant.safetyScore) }}>
             <h2>{restaurant.name}</h2>
             <p>Safety Score: {restaurant.safetyScore}</p>
             <div className="progress-bar">
